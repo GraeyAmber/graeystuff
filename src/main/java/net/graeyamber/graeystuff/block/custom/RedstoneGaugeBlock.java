2043d90d849439905e257fb212a58adea809e82c
GraeyAmber <bernardi.andrea.1999@gmail.com>
2023-11-22 17:20:27 +0100

    reworked redstone gauge logic

--- src/main/java/net/graeyamber/graeystuff/block/custom/RedstoneGaugeBlock.java
+++ src/main/java/net/graeyamber/graeystuff/block/custom/RedstoneGaugeBlock.java
@@ -1,9 +1,11 @@
 package net.graeyamber.graeystuff.block.custom;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
@@ -14,42 +16,37 @@ import net.minecraft.world.level.block.state.properties.IntegerProperty;
 import javax.annotation.Nullable;
 
 public class RedstoneGaugeBlock extends Block {
-
-    public static final IntegerProperty POWER = BlockStateProperties.POWER;
+    public static final IntegerProperty INPUT_POWER = BlockStateProperties.POWER;
     public RedstoneGaugeBlock(Properties pProperties) {
         super(pProperties);
-        this.registerDefaultState(this.defaultBlockState().setValue(POWER, Integer.valueOf(0)));
+        this.registerDefaultState(this.stateDefinition.any().setValue(INPUT_POWER, 0));
     }
 
     @Nullable
     public BlockState getStateForPlacement(BlockPlaceContext pContext) {
-        return this.defaultBlockState().setValue(POWER, Integer.valueOf(0));
+        pContext.getLevel().scheduleTick(pContext.getClickedPos(), this, 4);
+        return this.defaultBlockState();
     }
 
     public void neighborChanged(BlockState pState, Level pLevel, BlockPos pPos, Block pBlock, BlockPos pFromPos, boolean pIsMoving) {
         if (!pLevel.isClientSide) {
-            boolean flag = pState.getValue(POWER) > 0;
-            // if "powered value" not equals "neighbor signal"
-            if (flag != pLevel.hasNeighborSignal(pPos)) {
-                if (flag) {
-                    pLevel.scheduleTick(pPos, this, 4);
-                } else {
-                    pLevel.setBlock(pPos, pState.setValue(POWER, pLevel.getBestNeighborSignal(pPos)), 2);
-                }
+            if (pState.getValue(INPUT_POWER) != pLevel.getBestNeighborSignal(pPos)) {
+                pLevel.scheduleTick(pPos, this, 4);
             }
-
         }
     }
 
     public void tick(BlockState pState, ServerLevel pLevel, BlockPos pPos, RandomSource pRandom) {
-        // if powered but no neighbor signal
-        if (pState.getValue(POWER) > 0 && !pLevel.hasNeighborSignal(pPos)) {
-            pLevel.setBlock(pPos, pState.setValue(POWER, 0), 2);
+        int current = pState.getValue(INPUT_POWER);
+        int best = pLevel.getBestNeighborSignal(pPos);
+        if (current != best) {
+            pLevel.setBlock(pPos, pState.setValue(INPUT_POWER,
+                    (current > best) ? current - 1 : current + 1), 3);
+            pLevel.scheduleTick(pPos, this, 4);
         }
-
     }
 
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> pBuilder) {
-        pBuilder.add(POWER);
+        pBuilder.add(INPUT_POWER);
     }
 }
